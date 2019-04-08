b2536b262013eef54b28059089578e85e7a49f02
matcook <37397071+matcook@users.noreply.github.com>
2019-04-08 13:14:29 +0800

    uploaded files

---
+++ dist/js/script.js
@@ -0,0 +1,5 @@
+var envelope = document.querySelector(".envelope");
+envelope.addEventListener("click", function () {
+    envelope.classList.toggle("active");
+});
+//# sourceMappingURL=script.js.map
---
+++ script.ts
@@ -0,0 +1,5 @@
+let envelope: Element = document.querySelector(".envelope");
+
+envelope.addEventListener("click", () => {
+  envelope.classList.toggle("active");
+});
